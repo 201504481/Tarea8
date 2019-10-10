7e0775bfa46024258418bf5f598de0eadb1b0893
Julio Arango <eljulio.arango97@gmail.com>
2019-10-09 23:41:29 -0600

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -3,13 +3,13 @@ use knights;
 
 CREATE TABLE Materia (
   nombre VARCHAR(20),
-  codigo int, 
-  creditos int
+  codigo VARCHAR(20), 
+  creditos VARCHAR(20)
 );
 
 INSERT INTO Materia
   (nombre, codigo, creditos)
 VALUES
-  ('Compiladores2', 780, 5),
-  ('IPC1', 970, 5), 
-  ('Arqui 1', 960, 6);
+  ('Compiladores2', '780', '5'),
+  ('IPC1', '970', '5'), 
+  ('Arqui 1', '960', '6');
